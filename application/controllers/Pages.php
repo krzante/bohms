8ebb1a44314057f8f0b6ec1f1c176756b49897b9
krzante <kyleramon.zante@gmail.com>
2022-05-30 20:48:18 +0800

    Added pages folder, and temp header and footer

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,7 +4,7 @@
         Watch this to learn more: https://www.youtube.com/watch?v=I752ofYu7ag&list=PLillGF-RfqbaP_71rOyChhjeK1swokUIS&index=4&t=8s
     */
     class Pages extends CI_Controller{
-        public function view($page = 'settings'){
+        public function view($page = 'home'){
             if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
                 echo("nice");
                 show_404();
